773f6f89cca6c142a62a2bd306303765b1d6ae85
Еркежан <erketayrahymkozhieva@gmail.com>
2023-07-26 17:40:24 +0600

    This is a completed assigment

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String myStr = "Hello";
+        System.out.println(myStr.contains("Hel"));
+        System.out.println(myStr.contains("e"));
+        System.out.println(myStr.contains("Hi"));
+    }
+
+
+    String str1 = "Java string contains If else Main";
+    if(str1.contains("Hello"))
+      {
+        System.out.println("The Keyword :example: is found in given string");
+    } else
+
+    {
+        System.out.println("The Keyword :example: is not found in the string");
+    }
+}
+
+
